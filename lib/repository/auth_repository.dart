cfc75f04f894e5dd8a78d8457826129a3c255902
Anish Shrestha <anikshrestha23@gmail.com>
2024-02-13 12:51:00 +0545

    'api'

---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,26 @@
+import 'package:murarkey/Data/Network/base_api_services.dart';
+import 'package:murarkey/Data/Network/network_api_services.dart';
+import 'package:murarkey/res/app_url.dart';
+
+class AuthRepository {
+  BaseApiServices _apiServices = NetworkApiService();
+  Future<dynamic> LoginApi(dynamic data) async {
+    try {
+      dynamic response =
+          await _apiServices.getPostApiResponse(AppUrl.loginEndPoint, data);
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<dynamic> signUpapi(dynamic data) async {
+    try {
+      dynamic response =
+          await _apiServices.getApiResponse(AppUrl.registerApiEndpoint, data);
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
---
+++ lib/res/app_url.dart
@@ -0,0 +1,6 @@
+class AppUrl {
+  static var baseUrl = '';
+  static var loginEndPoint = baseUrl + '';
+  static var registerApiEndpoint = baseUrl + '';
+  
+}
